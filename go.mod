307f8a77c6eb457aa416b8fe0aa8a8f8c79aefaf
Brian Dwyer <brian.dwyer@broadridge.com>
2025-10-15 17:34:33 -0400

    Update and add support for Healthchecking

--- go.mod
+++ go.mod
@@ -1,15 +1,14 @@
 module aws-asg-status
 
-go 1.23.0
-
-toolchain go1.24.1
+go 1.24.0
 
 require (
 	github.com/aws/aws-sdk-go v1.55.8
+	github.com/mattn/go-isatty v0.0.20
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,17 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net"
+	"net/http"
 	"os"
+	"runtime"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -17,12 +23,24 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/mattn/go-isatty"
 )
 
+var healthcheckUrl string
+var healthcheckTimeout time.Duration
+
 func init() {
+	flag.StringVar(&healthcheckUrl, "healthcheck-url", "", "Healthcheck endpoint URL")
+	flag.DurationVar(&healthcheckTimeout, "healthcheck-timeout", 5*time.Minute, "Healthcheck timeout")
+
 	if _, ok := os.LookupEnv("AWS_ASG_STATUS_DEBUG"); ok {
 		log.SetLevel(log.DebugLevel)
 		log.SetReportCaller(true)
+	}
+	// Workaround for https://github.com/PowerShell/PowerShell/issues/14273
+	// PSNotApplyErrorActionToStderr
+	if runtime.GOOS == "windows" && !(isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())) {
+		log.SetOutput(os.Stdout)
 	}
 }
 
@@ -34,7 +52,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	if len(os.Args) == 1 {
+	if len(os.Args) == 1 && healthcheckUrl == "" {
 		log.Fatal("Must supply an argument: enter-standby|exit-standby|healthy|unhealthy|status")
 	}
 
@@ -64,7 +82,7 @@ func main() {
 
 	input := &ec2.DescribeTagsInput{
 		Filters: []*ec2.Filter{
-			&ec2.Filter{
+			{
 				Name: aws.String("resource-id"),
 				Values: []*string{
 					aws.String(instanceID),
@@ -110,7 +128,34 @@ func main() {
 
 	asClient := autoscaling.New(sess)
 
+	// Wait for Healthcheck if configured
+
 	switch os.Args[1] {
+	case "healthy", "":
+		status := aws.String("Healthy")
+		var err error
+		if healthcheckUrl != "" {
+			if err = waitUntilHealthy(); err != nil {
+				log.Error(err)
+				status = aws.String("Unhealthy")
+			}
+		}
+		if _, err = asClient.SetInstanceHealth(&autoscaling.SetInstanceHealthInput{
+			HealthStatus:             status,
+			InstanceId:               aws.String(instanceID),
+			ShouldRespectGracePeriod: aws.Bool(false),
+		}); err != nil {
+			log.Fatal(err)
+		}
+	case "unhealthy":
+		_, err := asClient.SetInstanceHealth(&autoscaling.SetInstanceHealthInput{
+			HealthStatus:             aws.String("Unhealthy"),
+			InstanceId:               aws.String(instanceID),
+			ShouldRespectGracePeriod: aws.Bool(false),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 	case "enter-standby":
 		standbyOut, err := asClient.EnterStandby(&autoscaling.EnterStandbyInput{
 			AutoScalingGroupName:           AsgName,
@@ -130,24 +175,6 @@ func main() {
 			log.Fatal(err)
 		}
 		prettyPrint(activeOut)
-	case "healthy":
-		_, err := asClient.SetInstanceHealth(&autoscaling.SetInstanceHealthInput{
-			HealthStatus:             aws.String("Healthy"),
-			InstanceId:               aws.String(instanceID),
-			ShouldRespectGracePeriod: aws.Bool(false),
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-	case "unhealthy":
-		_, err := asClient.SetInstanceHealth(&autoscaling.SetInstanceHealthInput{
-			HealthStatus:             aws.String("Unhealthy"),
-			InstanceId:               aws.String(instanceID),
-			ShouldRespectGracePeriod: aws.Bool(false),
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
 	case "status":
 		describeAsgsOut, err := asClient.DescribeAutoScalingGroups(&autoscaling.DescribeAutoScalingGroupsInput{
 			AutoScalingGroupNames: []*string{AsgName},
@@ -161,10 +188,81 @@ func main() {
 	}
 }
 
-func prettyPrint(obj interface{}) {
+func prettyPrint(obj any) {
 	prettyJSON, err := json.MarshalIndent(obj, "", "    ")
 	if err != nil {
 		log.Fatalln("Failed to Marshal JSON:", err)
 	}
 	fmt.Println(string(prettyJSON))
+}
+
+func waitUntilHealthy() error {
+	// Copy of http.DefaultTransport with Flippable TLS Verification
+	// https://golang.org/pkg/net/http/#Client
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: func() bool {
+				_, ok := os.LookupEnv("CFN_SIGNAL_SSL_VERIFY")
+				return ok
+			}()},
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), healthcheckTimeout)
+	defer cancel()
+
+	var bodyBytes []byte
+
+	for {
+		req, err := http.NewRequestWithContext(ctx, "GET", healthcheckUrl, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		requestTimeout := 30 * time.Second
+		rctx, rcancel := context.WithTimeout(ctx, requestTimeout)
+		defer rcancel()
+		resp, err := client.Do(req.WithContext(rctx))
+		if err != nil {
+			if ctxErr := ctx.Err(); ctxErr == context.DeadlineExceeded {
+				if len(bodyBytes) > 0 {
+					var prettyJSON bytes.Buffer
+					if err := json.Indent(&prettyJSON, bodyBytes, "", "  "); err != nil {
+						log.Error(string(bodyBytes))
+					} else {
+						log.Error(prettyJSON.String())
+					}
+				}
+				return fmt.Errorf("healthcheck exceeded timeout(%s): %w", healthcheckTimeout, err)
+			}
+			if ctxErr := rctx.Err(); ctxErr == context.DeadlineExceeded {
+				log.Warn(fmt.Errorf("healthcheck request timeout(%s): %w", requestTimeout, err))
+			} else {
+				log.Error(err)
+			}
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		defer resp.Body.Close()
+		switch resp.StatusCode {
+		case 200:
+			return nil
+		default:
+			log.Warnf("%v :: (%v) %v", healthcheckUrl, resp.StatusCode, resp.Status)
+			bodyBytes, _ = io.ReadAll(resp.Body)
+			resp.Body.Close()
+			time.Sleep(5 * time.Second)
+			continue
+		}
+	}
 }
